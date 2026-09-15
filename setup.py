55f055c70d29c6fca8b5137f600ddedd5735d425
Thomas Naselaris <99360340+nase0005@users.noreply.github.com>
2026-09-15 15:21:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import find_packages, setup
 setup(
     name="n_point_probes",
     version="0.1.0",
-    py_modules=["probes_experiment", "utils"],
+    py_modules=["probes_experiment", "utils", "models"],
 )
